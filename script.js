0005543d2aaf90eec15c8f0b9984cf951f109dab
ramazangalip <ramazansaidgalip@gmail.com>
2023-07-18 14:06:17 +0300

    18.07.2023

--- script.js
+++ script.js
@@ -5,29 +5,35 @@ function rastgeleSayi(min,max){
 }
 console.log(rastgeleSayi(1,10))
 
-// 2) elimizdeki people dizisiyle  people2 yi oluşturma
+// 2.soruda elimizde örnekte size verdiğim people dizisini kullanarak people2 ye dönüştürmeni istiyorum. people dizisinden bir şeyler çıkarıp ekleyerek people2 ye ulaşacaksın
 
-let people = ["Ramazan","Esma","Elisa"];
-let people2 = ["Seyfullah"]
-console.log(people.concat(people2))
+let people = ["Greg", "Mary", "Devon", "James"];
+// people2 = ["Greg", "Elizabeth", "Mary", "Artie", "James"]
 
-// 3) elimizdeki bir elemanı başka birşeye dönüştürme
+people.pop();
+people.pop();
+people.pop();
+people.push("Elizabeth","Mary","Artie","James")
+console.log(people)
 
-let texts = ["mustafa_selman@gmail.com ","mehmet_colak@hotmail.com ","yasin_sezer@yandex.com"]
 
-texts.splice(0,3,"Mustafa Selman","Mehmet Çolak","Yasir Sezer")
-console.log(texts)
+//3.soruda ise ben bir donusturucu() fonksiyonu yazmanı istedim. 
+var metinler = ["mustafa_selman@gmail.com","mehmet_colak@hotmail.com","yasin_sezer@yandex.com"]
+metinler.splice(0,3,"Mustafa Selman","Mehmet Çolak","Yasin Sezer")
+console.log(metinler)
 
-// 4) elimizdeki dizinin en büyük ikinci ve en küçük ikinci değerini yazdırma
+
+
+        
+
+// 4.soruda ise sayılar dizisindeki en büyük 2. değeri çıktı olarak veren bir fonksiyon ile en küçük ikinci değeri veren fonksiyon istiyorum sen sayıları önce sıralayıp sonrasında 2. en büyük ile 2. en küçüğü vereceksin
 
 let sayilar = [12,34,2,3,67,66,5,24]
 
-function ikinciEnBüyük(maxDeger,çıkarılcakDeger){
-    return(maxDeger-çıkarılcakDeger)
-}
-console.log(ikinciEnBüyük(67,1));
+let sayilar2 = sayilar.sort((a,b) =>b-a);
+console.log(sayilar2)
+console.log(sayilar2[1])
 
-function ikinciEnKüçük(minDeger,eklenecekDeger){
-    return(minDeger + eklenecekDeger)
-}
-console.log(ikinciEnKüçük(2,1))
+sayilar3 = sayilar2.reverse();
+
+console.log(sayilar3[1])
